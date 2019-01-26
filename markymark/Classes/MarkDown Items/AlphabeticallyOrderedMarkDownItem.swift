1ef902300341a593cf1aa8a65811dd4d2030998e
daisuke310vvv <daisuke.sato1991@gmail.com>
2019-01-27 01:24:50 +0900

    Make AlphabeticallyOrderedListMarkDownItem.indexCharacter public

--- markymark/Classes/MarkDown Items/AlphabeticallyOrderedMarkDownItem.swift
+++ markymark/Classes/MarkDown Items/AlphabeticallyOrderedMarkDownItem.swift
@@ -9,7 +9,7 @@ open class AlphabeticallyOrderedMarkDownItem: ListMarkDownItem {
 
     let indexCharacters: [String] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-    override var indexCharacter: String? {
+    override public var indexCharacter: String? {
         return "\(indexCharacters[index ?? 0])."
     }
 }
